8ea24ed83d3c2aa26aca6cc4e2a0c3fdc79b8a59
ABDULAZIZ BERENTO <abdulazizberento@ABDULAZIZs-MacBook-Air.local>
2022-11-21 13:54:39 -0600

    debugged 1.1

--- src/DebugOne1.java
+++ src/DebugOne1.java
@@ -1,2 +1,17 @@
-package PACKAGE_NAME;public class DebugOne1 {
+//initial version
+// public class DebugOne1
+//
+//        /* This program displays a greeting */
+//        public static void main(String[] args)
+//        {
+//            Systemoutprintl("Hello).
+//     }
+
+//debugged version
+public class DebugOne1 {
+
+        /* This program displays a greeting */
+        public static void main(String[] args) {
+            System.out.println("Hello");
+        }
 }
