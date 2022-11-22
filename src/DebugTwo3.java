cf29e2be1cba915d01e1b9935cb3321c22046d56
ABDULAZIZ BERENTO <abdulazizberento@ABDULAZIZs-MacBook-Air.local>
2022-11-21 23:03:06 -0600

    debugged 2.3 edited 1.3

---
+++ src/DebugTwo3.java
@@ -0,0 +1,29 @@
+//public class DebugTwo3
+//// Demonstrates remainder and output
+//{
+//    public static void main(String args[])
+//    {
+//        int a = 99, b = 8, result;
+//        long c = 7777777777777;
+//        result = a % b;
+//        System.out.println("Divide " + a + " by " + b);
+//        Systemout.println("remainder is " + a);
+//        Systemout.print("c is a very large number: ");
+//        Systemout.println(c);
+//    }
+//}
+public class DebugOne3{
+// Demonstrates remainder and output
+
+    public static void main(String[] args) {
+        int a = 99;
+        int b = 8;
+        int result;
+        double c = 7777777777777.0;
+        result = a % b;
+        System.out.println("Divide " + a + " by " + b);
+        System.out.println("remainder is " + a);
+        System.out.print("c is a very large number: ");
+        System.out.println(c);
+    }
+}
